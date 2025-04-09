7b1e12b48382925719460ccd47b93c7be697efb8
kimbank <kimeunhang@inha.edu>
2025-04-09 23:58:47 +0900

    add: Product, ProductBuying, ProductSelling 모델 선언

--- src/main/java/me/sallim/api/domain/product/model/Product.java
+++ src/main/java/me/sallim/api/domain/product/model/Product.java
@@ -3,21 +3,42 @@ package me.sallim.api.domain.product.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "product")
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
+@Table(name = "product")
 public class Product {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
     private Long id;
 
-    private String name;
+    @Column(name = "member_id")
+    private Long memberId;
 
-    @Column(length = 1000)
-    private String description;
+    @Column(name = "appliance_id")
+    private Long appliance_id;
 
-    private int price;
+    @Column(name = "title", length = 500)
+    private String title;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
+
+    @Column(name = "is_active", nullable = false, columnDefinition = "bit(1) default 1")
+    private Boolean isActive;
+
+    @Column(name = "post_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PostTypeEnum postType;
+
+    @Column(name = "product_photo_id")
+    private Long productPhotoId;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
 }
---
+++ src/main/java/me/sallim/api/domain/product/model/ProductPhoto.java
@@ -0,0 +1,26 @@
+package me.sallim.api.domain.product.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "product_photo")
+public class ProductPhoto {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_photo_id")
+    private Long id;
+
+    @Column(name = "product_id")
+    private Long productId;
+
+    @Column(name = "url", columnDefinition = "varchar(2048)")
+    private String url;
+
+    @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private String createdAt;
+}
