b6c96eb972f4491df3a3b704ccc684a84473e426
kimbank <kimeunhang@inha.edu>
2025-04-10 02:50:28 +0900

    feat: product 도메인 레포지토리 추가 및 query dsl 세팅

--- src/main/java/me/sallim/api/domain/product/repository/ProductRepository.java
+++ src/main/java/me/sallim/api/domain/product/repository/ProductRepository.java
@@ -1,13 +1,7 @@
 package me.sallim.api.domain.product.repository;
 
 import me.sallim.api.domain.product.model.Product;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    @EntityGraph(attributePaths = {"photos"})
-    Optional<Product> findWithPhotosById(Long id);
 }
