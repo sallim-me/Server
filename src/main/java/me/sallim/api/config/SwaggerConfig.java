74cb5d21ebd3c153521e6162e24f91d85ee77a16
kimbank <kimeunhang@inha.edu>
2025-05-22 14:40:15 +0900

    feat: 스웨거 개발 서버 url .env 관리

--- src/main/java/me/sallim/api/config/SwaggerConfig.java
+++ src/main/java/me/sallim/api/config/SwaggerConfig.java
@@ -8,11 +8,14 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.util.List;
 
+@Profile("dev")
 @Configuration
 @OpenAPIDefinition(
         info = @Info(title = "Sallim API", version = "1.0", description = "컴퓨터공학종합설계 : 제로투원팀의 서비스입니다."),
@@ -26,6 +29,9 @@ import java.util.List;
 )
 public class SwaggerConfig {
 
+    @Value("${spring.cors.allowed-origins}")
+    private String allowedOrigins;
+
     @Bean
     public OpenAPI customOpenAPI() {
         Server localServer = new Server()
@@ -33,7 +39,7 @@ public class SwaggerConfig {
                 .description("Local server");
 
         Server devServer = new Server()
-                .url("https://dev-back.sallim.me")
+                .url(allowedOrigins.split(",")[0])
                 .description("Dev server");
 
         return new OpenAPI().servers(List.of(devServer, localServer));
