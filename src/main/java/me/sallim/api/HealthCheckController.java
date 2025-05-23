b94dfff4a0001d7938131464f9e2adfe4f21a282
kimbank <kimeunhang@inha.edu>
2025-05-23 10:30:40 +0900

    refactor: SecurityConfig, HealthCheck 접근 방식

--- src/main/java/me/sallim/api/HealthCheckController.java
+++ src/main/java/me/sallim/api/HealthCheckController.java
@@ -1,6 +1,6 @@
 package me.sallim.api;
 
-import org.springframework.context.annotation.Profile;
+//import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +9,6 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-@Profile("dev")
 @RestController
 @RequestMapping("/health-check")
 public class HealthCheckController {
