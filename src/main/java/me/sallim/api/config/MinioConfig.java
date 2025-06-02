606f3ab2a9c8d2feb5ffb4e9e6f874c83288f326
kimbank <kimeunhang@inha.edu>
2025-06-03 01:54:35 +0900

    feat: 상품 사진 관리 기능 추가 및 S3 설정 적용

---
+++ src/main/java/me/sallim/api/config/MinioConfig.java
@@ -0,0 +1,34 @@
+package me.sallim.api.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+import java.net.URI;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${spring.minio.endpoint}")
+    private String endpoint;
+
+    @Value("${spring.minio.access-key}")
+    private String accessKey;
+
+    @Value("${spring.minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(StaticCredentialsProvider.create(
+                        AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.AP_SOUTH_1) // 실제로는 무시되지만 필요
+                .build();
+    }
+}
